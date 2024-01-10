b401060f0a57d78ee483b3efc3447c72e8cc1e4c
jinseoplee <jjjp9966@gmail.com>
2024-01-10 14:36:28 +0900

    chore: 데이터 초기화를 위한 data.sql 파일 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO article(title, content) VALUES ('article 1', 'content ...');
+INSERT INTO article(title, content) VALUES ('article 2', 'content ...');
+INSERT INTO article(title, content) VALUES ('article 3', 'content ...');
